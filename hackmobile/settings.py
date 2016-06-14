af794303f67cb9b6e84fd44a827e417870947090
yatuzhang <yatu.zhang@mail.utoronto.ca>
2016-06-14 01:19:10 -0400

    added hosts

--- hackmobile/settings.py
+++ hackmobile/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '0-wljtkzl+0n%m$x@n!1hi#$k_x3_)km+l!rc8w8zo#2e#$3wc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '172.31.21.142']
 
 # Application definition
 
