d988c44fa9014106fd638d74c121d59799451778
yatuzhang <yatu.zhang@mail.utoronto.ca>
2016-06-14 02:39:55 -0400

    updated

--- hackmobile/wsgi.py
+++ hackmobile/wsgi.py
@@ -12,11 +12,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-ppath = '/home/yatu/hackmobile_server/hackmobile'
-
-sys.path.append(ppath)
-os.environ.setdefault("PYTHONPATH", ppath)
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackmobile.settings")
 
 application = get_wsgi_application()
